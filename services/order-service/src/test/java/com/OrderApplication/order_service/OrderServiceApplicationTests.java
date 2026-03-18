b4c5027aa2c0a26ef5d71628a1074ea4d92b0023
Avishka Wijesinghe <avishkapraveen02@gmail.com>
2026-03-18 17:44:59 +0530

    Initiate

---
+++ services/order-service/src/test/java/com/OrderApplication/order_service/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.OrderApplication.order_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
